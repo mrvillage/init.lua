53376cbb610be70da6f6b5b61036569518c7f7d6
mrvillage <josef@mrvillage.dev>
2026-01-03 19:00:01 -0500

    Update config

--- init.lua
+++ init.lua
@@ -290,53 +290,53 @@ require('lazy').setup({
       version = '^5', -- Recommended
       lazy = false,   -- This plugin is already lazy
     },
-    -- {
-    --   "yetone/avante.nvim",
-    --   event = "VeryLazy",
-    --   version = false, -- Never set this value to "*"! Never!
-    --   opts = {
-    --   },
-    --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    --   build = "make",
-    --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    --   dependencies = {
-    --     "nvim-treesitter/nvim-treesitter",
-    --     "stevearc/dressing.nvim",
-    --     "nvim-lua/plenary.nvim",
-    --     "MunifTanjim/nui.nvim",
-    --     --- The below dependencies are optional,
-    --     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-    --     "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-    --     "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-    --     "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-    --     -- "zbirenbaum/copilot.lua",        -- for providers='copilot'
-    --     {
-    --       -- support for image pasting
-    --       "HakonHarnes/img-clip.nvim",
-    --       event = "VeryLazy",
-    --       opts = {
-    --         -- recommended settings
-    --         default = {
-    --           embed_image_as_base64 = false,
-    --           prompt_for_file_name = false,
-    --           drag_and_drop = {
-    --             insert_mode = true,
-    --           },
-    --           -- required for Windows users
-    --           use_absolute_path = true,
-    --         },
-    --       },
-    --     },
-    --     {
-    --       -- Make sure to set this up properly if you have lazy=true
-    --       'MeanderingProgrammer/render-markdown.nvim',
-    --       opts = {
-    --         file_types = { "markdown", "Avante" },
-    --       },
-    --       ft = { "markdown", "Avante" },
-    --     },
-    --   },
-    -- }
+    {
+      "yetone/avante.nvim",
+      event = "VeryLazy",
+      version = false, -- Never set this value to "*"! Never!
+      opts = {
+      },
+      -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+      build = "make",
+      -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+      dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        --- The below dependencies are optional,
+        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+        "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+        "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+        "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+        -- "zbirenbaum/copilot.lua",        -- for providers='copilot'
+        {
+          -- support for image pasting
+          "HakonHarnes/img-clip.nvim",
+          event = "VeryLazy",
+          opts = {
+            -- recommended settings
+            default = {
+              embed_image_as_base64 = false,
+              prompt_for_file_name = false,
+              drag_and_drop = {
+                insert_mode = true,
+              },
+              -- required for Windows users
+              use_absolute_path = true,
+            },
+          },
+        },
+        {
+          -- Make sure to set this up properly if you have lazy=true
+          'MeanderingProgrammer/render-markdown.nvim',
+          opts = {
+            file_types = { "markdown", "Avante" },
+          },
+          ft = { "markdown", "Avante" },
+        },
+      },
+    }
   },
   {
     git = {
@@ -784,6 +784,7 @@ vim.g.rustaceanvim = {
       vim.keymap.set('n', '<leader>rg', function()
         vim.cmd.RustLsp { 'crateGraph', '[backend]', '[output]' }
       end, { silent = true, buffer = bufnr, desc = '[R]ust Crate [G]raph' })
+      on_attach(_, bufnr)
     end,
     default_settings = {
       ['rust-analyzer'] = {
@@ -1095,25 +1096,27 @@ vim.keymap.set('n', '<leader>ps', ':terminal mrvillage run deploy-pnw -s<CR>')
 vim.keymap.set('n', '<leader>pp', ':terminal mrvillage run deploy-pnw -ps<CR>')
 
 -- avante
--- require("avante").setup({
---   debug = true,
---   provider = "claude",
---   -- provider = "ollama",
---   claude = {
---     endpoint = "https://api.anthropic.com",
---     model = "claude-sonnet-4-20250514",
---     -- model = "claude-3-7-sonnet-20250219",
---     temperature = 0,
---     max_tokens = 50000,
---   },
---   ollama = {
---     -- endpoint = "http://127.0.0.1:11434", -- Note that there is no /v1 at the end.
---     endpoint = "http://172.24.176.1:11434",
---     model = "qwq:latest",
---   },
--- })
+require("avante").setup({
+  debug = true,
+  provider = "claude",
+  -- provider = "ollama",
+  providers = {
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-sonnet-4-20250514",
+      -- model = "claude-3-7-sonnet-20250219",
+      -- temperature = 0,
+      -- max_tokens = 50000,
+    },
+    ollama = {
+      -- endpoint = "http://127.0.0.1:11434", -- Note that there is no /v1 at the end.
+      endpoint = "http://172.24.176.1:11434",
+      model = "qwq:latest",
+    },
+  }
+})
 
--- vim.keymap.set('n', '<leader>al', function() vim.cmd(':AvanteClear') end, { desc = '[A]vante C[l]ear' })
+vim.keymap.set('n', '<leader>al', function() vim.cmd(':AvanteClear') end, { desc = '[A]vante C[l]ear' })
 -- Some shortcuts to resolve common typos
 vim.api.nvim_create_user_command('W', 'w', {})
 vim.api.nvim_create_user_command('Q', 'q', {})
